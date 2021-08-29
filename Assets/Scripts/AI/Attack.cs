ad560fed03f1fdcfaaacf304523f89237f715486
Marlus Vinicius <marlusvinicius2014@outlook.com>
2021-08-29 10:19:03 -0300

    Adds basic attack and health scripts

---
+++ Assets/Scripts/AI/Attack.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack : MonoBehaviour
+{
+    [SerializeField] private float attackDamage;
+
+    private void OnCollisionEnter(Collision other)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Assets/Scripts/Entities/Health.cs
@@ -0,0 +1,33 @@
+﻿using Unity.Collections;
+using UnityEngine;
+
+namespace Entities
+{
+    public class Health : MonoBehaviour
+    {
+        [SerializeField] private int maxHealth = 100;
+        [SerializeField] private int startingHealth = 100;
+        
+        private int _currentHealth;
+
+        private void Awake()
+        {
+            _currentHealth = Mathf.Clamp(startingHealth, 0, maxHealth);
+        }
+
+        public void TakeDamage(int damage)
+        {
+            _currentHealth -= damage;
+            if (_currentHealth < 0)
+            {
+                _currentHealth = 0;
+                Die();
+            }
+        }
+        
+        private void Die()
+        {
+            Destroy(gameObject);
+        }
+    }
+}
