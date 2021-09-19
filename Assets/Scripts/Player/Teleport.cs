fbe3a347c64e37ed117fa256dd80ae38910ea930
Marlus Vinicius <marlusvinicius2014@outlook.com>
2021-09-19 20:35:39 -0300

    Implements fully functional portal.
    Needs some adjustments

--- Assets/Scripts/Player/Teleport.cs
+++ Assets/Scripts/Player/Teleport.cs
@@ -1,23 +1,38 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class Teleport : MonoBehaviour
 {
     [SerializeField] private GameObject[] portals;
     [SerializeField] private float range = 3f;
-    private KeyCode activateKey = KeyCode.E;
+    [SerializeField] private KeyCode activateKey = KeyCode.E;
+    [TextArea][SerializeField] private string actionMessage;
+    [SerializeField] private TextMeshProUGUI actionMessageHUD;
 
+    private bool _canBeInteractedWith;
+
+
+    private void Awake()
+    {
+        actionMessageHUD.text = actionMessage;
+    }
+    
     private void Update()
     {
         foreach (var portal in portals)
         {
-            if (Vector3.Distance(gameObject.transform.position, portal.transform.position) < range)
+            if (Vector3.SqrMagnitude(portal.transform.position - gameObject.transform.position) < range*range)
             {
-                print("press " + activateKey + " to use");
+                actionMessageHUD.enabled = _canBeInteractedWith = true;
                 if (Input.GetKey(activateKey))
-                    gameObject.transform.localPosition = Vector3.zero;
+                    SceneUtil.ResetScene();
+            }
+            if (Vector3.SqrMagnitude(portal.transform.position - gameObject.transform.position) > range*range)
+            {
+                actionMessageHUD.enabled = _canBeInteractedWith =  false;
             }
         }
     }
