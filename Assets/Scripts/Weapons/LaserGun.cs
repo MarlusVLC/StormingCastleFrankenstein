e014acb5e9501ca8a0ac41ae437e0c9ce1847151
Marlus Vinicius <marlusvinicius2014@outlook.com>
2021-09-28 22:28:53 -0300

    Basic VFX implemented

--- Assets/Scripts/Weapons/LaserGun.cs
+++ Assets/Scripts/Weapons/LaserGun.cs
@@ -11,10 +11,7 @@ namespace Weapons
     public class LaserGun : Gun
     {
         // laser
-        [SerializeField] private VisualEffect vfxGhostgunLoop;
-        [SerializeField] private VisualEffect vfxGhostgunFadeIn;
-        [SerializeField] private VisualEffect vfxGhostgunFadeOut;
-        [SerializeField] private Material material;
+        [SerializeField] private VisualEffect _visualEffect;
         // private LineRenderer line;
         private bool isVfxGhostgunPlaying;
         private bool canPlayVfxGhostGunStart;
@@ -31,24 +28,17 @@ namespace Weapons
             canPlayVfxGhostGunStart = true;
             
             
-            vfxGhostgunLoop.Stop();
-            vfxGhostgunFadeIn.Stop();
-            vfxGhostgunFadeOut.Stop();
-
-            vfxGhostgunFadeOut.playRate = 2f;
+            _visualEffect.Stop();
         }
 
-        private void Update()
-        {
-            if (isVfxGhostgunPlaying && 
-                (Input.GetMouseButtonUp(0) || IsEmpty))
-            {
-                vfxGhostgunLoop.Stop();
-                vfxGhostgunFadeIn.Stop();
-                StopCoroutine(PlayVfxGhostGun());
-                StartCoroutine(StopVfxGhostGun());
-            }
-        }
+        // private void Update()
+        // {
+        //     if (isVfxGhostgunPlaying && 
+        //         (Input.GetMouseButtonUp(0) || IsEmpty))
+        //     {
+        //        StopVfxGhostGun();
+        //     }
+        // }
         
         public override Gun PullTrigger(bool shooting, Ray ray, LayerMask damageableLayer)
         {
@@ -56,13 +46,13 @@ namespace Weapons
             if (_readyToShoot && shooting && _bulletsLeft > 0)
             {
                 Shoot(shooting, ray, damageableLayer);
-                StartCoroutine(PlayVfxGhostGun());
-                StopCoroutine(StopVfxGhostGun());
+                if (isVfxGhostgunPlaying == false)
+                    PlayVfxGhostGun();
                 _weaponAudio.ShotWithShell();
             }
             
-            // if (!shooting || _bulletsLeft <= 0) 
-                // line.enabled = false;
+            if (isVfxGhostgunPlaying && (!shooting || IsEmpty)) 
+                StopVfxGhostGun();
 
             return this;
         }
@@ -85,19 +75,6 @@ namespace Weapons
             {
                 targetPoint = ray.GetPoint(75);
             }
-            
-            // instantiate laser
-            // if (shooting)
-            // {
-            //     line.enabled = true;
-            //     line.SetPosition(0, attackPosition);
-            //     line.SetPosition(1, targetPoint);
-            //     line.material = material;
-            //     line.shadowCastingMode = ShadowCastingMode.On;
-            // }
-            
-            
-            // if (canPlayVfxGhostGunStart) StartCoroutine(PlayVfxGhostGun());
 
             _bulletsLeft = (int) (_bulletsLeft - 1 * Time.deltaTime);
             OnAmmoChanged();
@@ -112,38 +89,16 @@ namespace Weapons
             }
         }
 
-        private IEnumerator PlayVfxGhostGun()
+        private void PlayVfxGhostGun()
         {
-            if (CanPlayLaserVFX)
-            {
-                vfxGhostgunFadeIn.enabled = true;
-                isVfxGhostgunPlaying = true;
-                print("started coroutine");
-                // canPlayVfxGhostGunStart = false;
-                vfxGhostgunFadeIn.Play();
-
-                yield return new WaitForSeconds(0.1f);
-
-                while (isVfxGhostgunPlaying)
-                {
-                    vfxGhostgunLoop.enabled = true;
-                    vfxGhostgunLoop.Play();
-                    yield return new WaitForSeconds(0.1f);
-                }
-            }
+           _visualEffect.Play();
+           isVfxGhostgunPlaying = true;
         }
 
-        private IEnumerator StopVfxGhostGun()
+        private void StopVfxGhostGun()
         {
-            vfxGhostgunFadeOut.enabled = true;
-            vfxGhostgunFadeOut.Play();
-            yield return new WaitForSeconds(2);
+            _visualEffect.Stop();
             isVfxGhostgunPlaying = false;
-            Debug.Log(isVfxGhostgunPlaying);
-
-            vfxGhostgunLoop.enabled = false;
-            vfxGhostgunFadeIn.enabled = false;
-            vfxGhostgunFadeOut.enabled = false;
         }
 
         public override int ShotsLeft => 100*_bulletsLeft/magazineSize;
