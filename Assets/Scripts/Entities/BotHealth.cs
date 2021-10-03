7aadb5c75da312bb6a6a275210a450346c0e1365
Choosecake <imchoosecake@gmail.com>
2021-10-03 20:30:01 -0300

    changes a few things

--- Assets/Scripts/Entities/BotHealth.cs
+++ Assets/Scripts/Entities/BotHealth.cs
@@ -1,12 +1,25 @@
 ﻿using Audio;
+using UnityEngine;
+using UnityEngine.VFX;
 
 namespace Entities
 {
     public class BotHealth : Health
     {
+        [SerializeField] private VisualEffect bloodVFX;
+
+        private void Awake()
+        {
+            bloodVFX.Stop();
+        }
+
         protected override void Die()
         {
-            Destroy(this.gameObject);
+            transform.parent.transform.position = transform.position;
+            Instantiate(bloodVFX, transform.parent);
+            bloodVFX.Play();
+            
+            Destroy(gameObject);
             
             FindObjectOfType<WendigoSound>().PlayWendigoDeathSound();
         }
